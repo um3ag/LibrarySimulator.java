b711ba7bb1c24aee05b5b421887a7734461b4142
um3ag <119005069+um3ag@users.noreply.github.com>
2025-10-17 16:15:52 +0300

    Add files via upload

---
+++ LibrarySimulator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+// Bilal Bazraa    446109487
+// Abdullah Fatani 446102126
+// Badr Alsalim    446107156
+
+
+
+public class LibrarySimulator {
+    public static void main(string args[]){
+        Scanner input = new Scanner(system.in);
+
+
+
+    }
+}
