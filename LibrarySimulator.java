7eefbe98a298748935d89a78bbfa0a2cafc5115a
Bilo566 <bazaraabilal@gmail.com>
2025-10-17 16:33:15 +0300

    Update LibrarySimulator.java

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -9,6 +9,27 @@ public class LibrarySimulator {
     public static void main(string args[]){
         Scanner input = new Scanner(system.in);
 
+        // User predefined accounts
+        // Includes: ID, Name, Borrowed books, Total session data and fees
+       
+        int user1_ID = "446109487";
+        String user1_name = "Bilal";
+        int user1_borrowed_books = 0;
+        double user1_fees = 0;
+
+        
+         int user2_ID = "446109487";
+        String user2_name = "Bilal";
+        int user2_borrowed_books = 0;
+        double user2_fees = 0;
+
+         int user3_ID = "446109487";
+        String user3_name = "Bilal";
+        int user3_borrowed_books = 0;
+        double user3_fees = 0;
+
+        
+
 
 
     }
@@ -41,3 +62,4 @@ Code style:
 - Use clear variable names
 - Indent properly
 - Add short comments for key parts*/
+
