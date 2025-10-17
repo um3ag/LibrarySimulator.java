0bf8db5b2cf2d407cab1e3ebf22826df6a4a536f
um3ag <119005069+um3ag@users.noreply.github.com>
2025-10-17 16:41:03 +0300

    Update LibrarySimulator.java
    - updating notes

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -47,30 +47,16 @@ public class LibrarySimulator {
 
 
 
-/* Note (Rules and Constraints) :
-- You must use only concepts covered in the course.
-- Do not use arrays.
-- Do not use multiple classes (this version must be implemented in a single Java class).
-- Format outputs using printf, \t, or substring to ensure:
-
-Two decimal places for monetary values
-
-Appropriate and readable output
-- The program must not allow borrowing more than 5 books or returning books if none are borrowed.
-- If the user attempts an invalid operation (e.g., borrowing the 6th book), show an appropriate error message.
+/* 
 Submission Rules:
-
-Submit your solution as LibrarySimulator.java on LMS
-
-The submitted Java file must include the names and ID numbers of all group members at the top as a comment
-
-Only one student of each group needs to submit the project on behalf of the whole group
-
-Code must compile and run with javac LibrarySimulator.java and java LibrarySimulator
-
-Code style:
-- Use clear variable names
-- Indent properly
-- Add short comments for key parts*/
-
-
+ 
+    - Submit your solution as LibrarySimulator.java on LMS
+    - The submitted Java file must include the names and ID numbers of all group members at the top as a comment
+    - Only one student of each group needs to submit the project on behalf of the whole group
+    - Code must compile and run with javac LibrarySimulator.java and java LibrarySimulator
+    
+    - Code style:
+        - Use clear variable names
+        - Indent properly
+        - Add short comments for key parts
+*/
