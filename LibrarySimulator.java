01cad61108d687c73699e7c45a532a54d0de96ba
Bilo566 <bazaraabilal@gmail.com>
2025-10-17 16:42:31 +0300

    Refactor comments and rules in LibrarySimulator
    Updated comments and clarified rules for the LibrarySimulator.

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -34,7 +34,7 @@ public class LibrarySimulator {
         int total_return = 0;
 
         // Main menu loop: for the interface
-        //while loop
+        // we use a whilw loop so that the expression when using the interface is always true. Asking the user to select the option
         while(true) {
         
 
@@ -47,16 +47,32 @@ public class LibrarySimulator {
 
 
 
-/* 
+/* Note (Rules and Constraints) :
+- You must use only concepts covered in the course.
+- Do not use arrays.
+- Do not use multiple classes (this version must be implemented in a single Java class).
+- Format outputs using printf, \t, or substring to ensure:
+
+Two decimal places for monetary values
+
+Appropriate and readable output
+- The program must not allow borrowing more than 5 books or returning books if none are borrowed.
+- If the user attempts an invalid operation (e.g., borrowing the 6th book), show an appropriate error message.
 Submission Rules:
- 
-    - Submit your solution as LibrarySimulator.java on LMS
-    - The submitted Java file must include the names and ID numbers of all group members at the top as a comment
-    - Only one student of each group needs to submit the project on behalf of the whole group
-    - Code must compile and run with javac LibrarySimulator.java and java LibrarySimulator
-    
-    - Code style:
-        - Use clear variable names
-        - Indent properly
-        - Add short comments for key parts
-*/
+
+Submit your solution as LibrarySimulator.java on LMS
+
+The submitted Java file must include the names and ID numbers of all group members at the top as a comment
+
+Only one student of each group needs to submit the project on behalf of the whole group
+
+Code must compile and run with javac LibrarySimulator.java and java LibrarySimulator
+
+Code style:
+- Use clear variable names
+- Indent properly
+- Add short comments for key parts*/
+
+
+
+
